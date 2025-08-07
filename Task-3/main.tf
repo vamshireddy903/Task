c25ebf86d3aeb49e712da780f89d15db1671ee48
Vamsi <devopsaws925@gmail.com>
2025-08-07 16:15:49 +0530

    Create main.tf

---
+++ Task-3/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  name  = "nginx-container"
+  image = docker_image.nginx.name
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
